d71ee236d58b50f794431291177878847f4a76c3
Hiei32 <451089132@qq.com>
2021-02-01 13:14:38 +0800

    组件添加修改

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,23 +3,38 @@
     <nav-bar class="home-nav">
       <template #center>首页</template>
     </nav-bar>
-    <div>首页</div>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView"
 import { getHomeMultidata } from "network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
   },
   created() {
     // 1.请求多个数据
     getHomeMultidata().then((res) => {
-      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
     });
   },
 };
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="recommends">
+    <a
+      href=""
+      v-for="(item, index) in recommends"
+      :key="index"
+      :src="item.link"
+    >
+      <img :src="item.image" alt="" />
+      <span>{{ item.title }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.recommends {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  padding: 10px 0;
+  margin-bottom: 10px;
+
+  a {
+    flex: 1;
+    text-align: center;
+
+    img {
+      width: 64px;
+      height: 64px;
+      margin-bottom: 4px;
+    }
+
+    span {
+      width: 100%;
+      height: 20px;
+      line-height: 20px;
+      text-align: center;
+      display: inline-block;
+      font-size: 14px;
+    }
+  }
+}
+</style>
